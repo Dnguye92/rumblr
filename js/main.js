0beb512d2e83fdefc3cafe3137ec74e8d3d50da4
Dylan Nguyen <dnguye92@Yahoo.com>
2017-02-20 18:52:19 -0800

    added event listener for post function

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const addPostInput = document.querySelector('input.addPostInput');
+const addTextArea = document.querySelector('textarea.addPostInput');
+const addPostButton = document.querySelector('button.addPostButton');
+
+addPostButton.addEventListener('click', () => {
+	let div = document.getElementsByClassName('fullPost')[0];
+	let header = document.createElement('h1');
+	let p = document.createElement('p');
+	header.textContent = addPostInput.value;
+	p.textContent = addTextArea.value;
+	div.appendChild(header);
+	div.appendChild(p);
+	addPostInput.value = '';
+	addTextArea.value = '';
+});
