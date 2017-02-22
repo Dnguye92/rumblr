a7ce2d6abc676163e093d5cace92f9fce6630d5b
Dylan Nguyen <dnguye92@Yahoo.com>
2017-02-21 18:37:54 -0800

    Finding right  method to append buttons to div

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//declare constants for textarea, button, and input
 const addPostInput = document.querySelector('input.addPostInput');
 const addTextArea = document.querySelector('textarea.addPostInput');
 const addPostButton = document.querySelector('button.addPostButton');
@@ -9,14 +10,29 @@ addPostButton.addEventListener('click', () => {
 	postBody.className = 'postBody';
 
 	let header = document.createElement('h1');
+	header.textContent = addPostInput.value;
 
 	let p = document.createElement('p');
-
-	header.textContent = addPostInput.value;
 	p.className = 'bodyContent';
 	p.textContent = addTextArea.value;
+
+	let up = document.createElement('button');
+	up.className = 'up';
+	up.innerHTML = 'Up';
+
+	let down = document.createElement('button');
+	down.className = 'down';
+	down.innerHTML = 'Down';
+
+	let remove = document.createElement('button');
+	remove.className = 'remove';
+	remove.innerHTML = 'Remove';
+
 	postBody.appendChild(header);
 	postBody.appendChild(p);
+	postBody.appendChild(up);
+	postBody.appendChild(down);
+	postBody.appendChild(remove);
 	body.appendChild(postBody);
 
 	addPostInput.value = '';
