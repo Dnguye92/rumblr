341994ecac23ea8d6f0e40d8564857ed6838e4ac
Dylan Nguyen <dnguye92@Yahoo.com>
2017-02-21 19:18:04 -0800

    create div for buttons

--- js/main.js
+++ js/main.js
@@ -1,41 +1,94 @@
 //declare constants for textarea, button, and input
+// const addPostInput = document.querySelector('input.addPostInput');
+// const addTextArea = document.querySelector('textarea.addPostInput');
+// const addPostButton = document.querySelector('button.addPostButton');
+
+//event listener for posting functionality
+// addPostButton.addEventListener('click', () => {
+// 	let body = document.getElementsByTagName('body')[0];
+
+// 	let postBody = document.createElement('div');
+// 	postBody.className = 'postBody';
+
+// 	let header = document.createElement('h1');
+// 	header.textContent = addPostInput.value;
+
+// 	let p = document.createElement('p');
+// 	p.className = 'bodyContent';
+// 	p.textContent = addTextArea.value;
+
+// 	let up = document.createElement('button');
+// 	up.className = 'up';
+// 	up.innerHTML = 'Up';
+
+// 	let down = document.createElement('button');
+// 	down.className = 'down';
+// 	down.innerHTML = 'Down';
+
+// 	let remove = document.createElement('button');
+// 	remove.className = 'remove';
+// 	remove.innerHTML = 'Remove';
+
+// 	postBody.appendChild(header);
+// 	postBody.appendChild(p);
+// 	postBody.appendChild(up);
+// 	postBody.appendChild(down);
+// 	postBody.appendChild(remove);
+// 	body.appendChild(postBody);
+
+// 	addPostInput.value = '';
+// 	addTextArea.value = '';
+// });
+
+//declare constants for textarea, button, and input
 const addPostInput = document.querySelector('input.addPostInput');
 const addTextArea = document.querySelector('textarea.addPostInput');
 const addPostButton = document.querySelector('button.addPostButton');
 
 //event listener for posting functionality
 addPostButton.addEventListener('click', () => {
-	let body = document.getElementsByTagName('body')[0];
+let body = document.getElementsByTagName('body')[0];
 
-	let postBody = document.createElement('div');
-	postBody.className = 'postBody';
+let postBody = document.createElement('div');
+postBody.className = 'postBody';
 
-	let header = document.createElement('h1');
-	header.textContent = addPostInput.value;
+let header = document.createElement('h1');
+header.textContent = addPostInput.value;
 
-	let p = document.createElement('p');
-	p.className = 'bodyContent';
-	p.textContent = addTextArea.value;
+let p = document.createElement('p');
+p.className = 'bodyContent';
+p.textContent = addTextArea.value;
 
-	let up = document.createElement('button');
-	up.className = 'up';
-	up.innerHTML = 'Up';
+//create div for buttons
+let buttonDiv = document.createElement('div');
+buttonDiv.className = 'buttonDiv'
 
-	let down = document.createElement('button');
-	down.className = 'down';
-	down.innerHTML = 'Down';
+let up = document.createElement('button');
+up.className = 'up';
+up.innerHTML = 'Up';
 
-	let remove = document.createElement('button');
-	remove.className = 'remove';
-	remove.innerHTML = 'Remove';
+let down = document.createElement('button');
+down.className = 'down';
+down.innerHTML = 'Down';
 
-	postBody.appendChild(header);
-	postBody.appendChild(p);
-	postBody.appendChild(up);
-	postBody.appendChild(down);
-	postBody.appendChild(remove);
-	body.appendChild(postBody);
+let remove = document.createElement('button');
+remove.className = 'remove';
+remove.innerHTML = 'Remove';
 
-	addPostInput.value = '';
-	addTextArea.value = '';
+postBody.appendChild(header);
+postBody.appendChild(p);
+//postBody.appendChild(up);
+//postBody.appendChild(down);
+//postBody.appendChild(remove);
+
+
+buttonDiv.appendChild(up);
+buttonDiv.appendChild(down);
+buttonDiv.appendChild(remove);
+
+body.appendChild(postBody);
+body.appendChild(buttonDiv)
+
+addPostInput.value = '';
+addTextArea.value = '';
 });
