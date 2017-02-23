f06134c90bb2854a0644d5ccb006095090dea234
Dylan Nguyen <dnguye92@Yahoo.com>
2017-02-22 19:17:12 -0800

    add scale animation to buttons

--- js/main.js
+++ js/main.js
@@ -1,46 +1,4 @@
 //declare constants for textarea, button, and input
-// const addPostInput = document.querySelector('input.addPostInput');
-// const addTextArea = document.querySelector('textarea.addPostInput');
-// const addPostButton = document.querySelector('button.addPostButton');
-
-//event listener for posting functionality
-// addPostButton.addEventListener('click', () => {
-// 	let body = document.getElementsByTagName('body')[0];
-
-// 	let postBody = document.createElement('div');
-// 	postBody.className = 'postBody';
-
-// 	let header = document.createElement('h1');
-// 	header.textContent = addPostInput.value;
-
-// 	let p = document.createElement('p');
-// 	p.className = 'bodyContent';
-// 	p.textContent = addTextArea.value;
-
-// 	let up = document.createElement('button');
-// 	up.className = 'up';
-// 	up.innerHTML = 'Up';
-
-// 	let down = document.createElement('button');
-// 	down.className = 'down';
-// 	down.innerHTML = 'Down';
-
-// 	let remove = document.createElement('button');
-// 	remove.className = 'remove';
-// 	remove.innerHTML = 'Remove';
-
-// 	postBody.appendChild(header);
-// 	postBody.appendChild(p);
-// 	postBody.appendChild(up);
-// 	postBody.appendChild(down);
-// 	postBody.appendChild(remove);
-// 	body.appendChild(postBody);
-
-// 	addPostInput.value = '';
-// 	addTextArea.value = '';
-// });
-
-//declare constants for textarea, button, and input
 const addPostInput = document.querySelector('input.addPostInput');
 const addTextArea = document.querySelector('textarea.addPostInput');
 const addPostButton = document.querySelector('button.addPostButton');
@@ -53,6 +11,7 @@ let postBody = document.createElement('div');
 postBody.className = 'postBody';
 
 let header = document.createElement('h1');
+header.className = 'postHeader';
 header.textContent = addPostInput.value;
 
 let p = document.createElement('p');
@@ -77,10 +36,6 @@ remove.innerHTML = 'Remove';
 
 postBody.appendChild(header);
 postBody.appendChild(p);
-//postBody.appendChild(up);
-//postBody.appendChild(down);
-//postBody.appendChild(remove);
-
 
 buttonDiv.appendChild(up);
 buttonDiv.appendChild(down);
